84ea1079be61d500052baf3c87b08575cbabe383
AlexsJones <alexsimonjones@gmail.com>
2026-02-15 18:25:09 +0000

    Initial homebrew tap for llmfit

---
+++ Formula/llmfit.rb
@@ -0,0 +1,34 @@
+class Llmfit < Formula
+  desc "Terminal tool that right-sizes LLM models to your system hardware"
+  homepage "https://github.com/AlexsJones/llmfit"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/AlexsJones/llmfit/releases/download/v#{version}/llmfit-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/AlexsJones/llmfit/releases/download/v#{version}/llmfit-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/AlexsJones/llmfit/releases/download/v#{version}/llmfit-v#{version}-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/AlexsJones/llmfit/releases/download/v#{version}/llmfit-v#{version}-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "llmfit"
+  end
+
+  test do
+    assert_match "llmfit", shell_output("#{bin}/llmfit --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# Homebrew Tap for llmfit
+
+Install [llmfit](https://github.com/AlexsJones/llmfit) via Homebrew.
+
+## Usage
+
+```sh
+brew tap AlexsJones/llmfit
+brew install llmfit
+```
+
+## Update
+
+```sh
+brew update
+brew upgrade llmfit
+```
